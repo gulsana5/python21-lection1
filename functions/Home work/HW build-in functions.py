c223dbb2d80c20709b6bae6ebfdd402a9ac6ef42
gulsana <aidaadarchi5@gmail.com>
2022-06-30 16:31:55 +0600

    codid

---
+++ functions/Home work/HW build-in functions.py
@@ -0,0 +1,99 @@
+
+#   Task 1
+# import functools
+# list_ = [1, 2, 3, 4] 
+# result = functools.reduce(lambda x, y: x + y, list_)
+# print(result)
+
+# Task 2
+# list_ = [1, 5, -9, 6, -4] 
+# result = all(a > 3 for a in list_)
+# print(result)
+
+# Task 3
+# list_ = [5, 8, 4, 6, 7]
+# result = any(a < 0 for a in list_)
+# print(result)
+
+# Task 4 
+# list_ = [1, 2, 3, 4]  
+# result = list(map(lambda x : x ** 2, list_))
+# print(result)
+
+# Task 5
+# list_ = [1, 2, 3, 4] 
+# result = list(filter(lambda x : x % 2  == 0 , list_))
+# print(result)
+
+# Task 6
+# list_ = ['Gulsana', 'python', 'internet', 'gool']
+# result = list(filter(lambda a: len(a) > 7 , list_))
+# print(result)
+
+# Task 7
+# list_ = [5, 6, 7, 8] 
+# from functools import reduce 
+# result = reduce(lambda a, b : a * b , list_)
+# print(result)
+
+# Task 8
+# list_ = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] 
+# list2 =list(filter(lambda a : a % 2 == 0, list_))
+# list3 = list(filter(lambda a : a % 2 != 0, list_))
+# result = (f'even: {len(list2)}, odd: {len(list3)}')
+# print(result)
+
+# Task 9
+# list_ = [-1, 2, 3, 5, -3, 7] 
+# result = list(map(lambda a: a > 0 , list_))
+# print(result)
+
+# Task 10
+# from functools import reduce
+# list_ = ['Paul', 'George', 'Ringo', 'John'] 
+# result = reduce(lambda a, b: a if len(a) > len(b) else b, list_)
+# print(result)
+
+# ==================================================================
+# Типы данных чиcла
+
+# Task 1
+# num = 23
+# print(num)
+
+# Task 2
+# word = 'hello'
+# print(word)
+
+# Task 3
+# number = 2
+# string = ('bootcamp')
+# print(string * number)
+
+# Task 4
+# x = 23
+# y = 35
+# print(x + y)
+
+# Task 5
+# x = 44
+# y = 4
+# print(x / y) 
+
+#  Task 6
+#    
+import random
+
+def hangman():
+    
+    wold_ = ['вишня', 'картошка', 'питачок', 'арбуз']
+    x = random.choice(wold_)
+    y = 'уеаиоыйя'
+    baraban = 5
+    
+    while baraban > 0:
+        missed = 0
+        for letter in x:
+            print(letter,end=' ')misse
+
+
