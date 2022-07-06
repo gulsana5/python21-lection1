db63a358989b6fdfd4b6a5f34d91af50fd5e55fa
gulsana <aidaadarchi5@gmail.com>
2022-07-06 10:28:12 +0600

    Poli[masterbc12dad]poli

--- oop/concepts.py
+++ oop/concepts.py
@@ -5,3 +5,25 @@
 # Абстракция
 # Ассоциация
 
+import pygame
+
+pygame.init()
+
+
+dis = pygame.display.set_mode((500, 500))
+pygame.display.set_caption('Змейка на питоне')
+
+blue = (0,0,350)
+red = (350,0,0)
+
+game_over = False
+while not game_over:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            game_over = True
+    pygame.draw.rect(dis,blue,[200,150,10,0])
+    pygame.display.update()
+pygame.quit()
+quit()
+
+
